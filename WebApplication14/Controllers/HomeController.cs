efd914a96694e8c1ff4d2bb99d027882d55655eb
Avrumi Friedman <litw13gh@gmail.com>
2026-03-23 21:58:06 -0400

    add homework

--- WebApplication14/Controllers/HomeController.cs
+++ WebApplication14/Controllers/HomeController.cs
@@ -54,3 +54,27 @@ namespace WebApplication14.Controllers
         }
     }
 }
+
+/*
+* Create an application that allows users to post free stuff. 
+* 
+* On the home page, there should be a list of all the postings, sorted by date 
+* descending (most recent first). 
+* 
+* There should then be a second page, that allows the user to post a new listing. A 
+* listing should have: DateCreated, Text, Name (optional) and a Phone Number. 
+* 
+* Since we're not creating any kind of login system, we need to give users the ability
+* to delete their listings. The way this will be achieved is via Session. E.g.
+* In Session, we'll be storing a List<int> that will keep track of all the ids posted
+* by the current user. Therefore, every time someone posts a listing, we'll add that new id
+* to that list.
+*
+* Then, when the home page is shown, for every listing we're showing, if that listing is in 
+* the session List<int>, a delete button should be shown next to the posting,
+* which when clicked, should delete the post. (Alternatively, this can be done in a cookie
+* where you store the ids as a comma separated string of ids e.g. (1,2,23,4) -- do this for EC)
+* 
+* 
+* Good luck!
+*/
